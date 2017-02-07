b7ee0c8cc4dbe334248d750fe360ca683fdb8cb9
sk8withaguitar <Felix.Ramirezjr@gmail.com>
2017-02-07 06:37:55 -0800

    Uploading Changes...

--- public/javascripts/twitter.js
+++ public/javascripts/twitter.js
@@ -4,8 +4,7 @@ var TimerExample = React.createClass({
 
         // This is called before our render function. The object that is
         // returned is assigned to this.state, so we can use it later.
-
-        return { elapsed: 0 };
+        return {dataSent: 0}
     },
 
     componentDidMount: function(){
@@ -13,7 +12,7 @@ var TimerExample = React.createClass({
         // componentDidMount is called by react when the component
         // has been rendered on the page. We can set the interval here:
 
-        this.timer = setInterval(this.tick, 50);
+        //this.timer = setInterval(this.tick, 50);
     },
 
     componentWillUnmount: function(){
@@ -21,23 +20,17 @@ var TimerExample = React.createClass({
         // This method is called immediately before the component is removed
         // from the page and destroyed. We can clear the interval here:
 
-        clearInterval(this.timer);
-    },
-
-    tick: function(){
-
-        // This function is called every 50 ms. It updates the
-        // elapsed counter. Calling setState causes the component to be re-rendered
-
-        this.setState({elapsed: new Date() - this.props.start});
+        //clearInterval(this.timer);
     },
 
     render: function() {
 
-        var elapsed = Math.round(this.state.elapsed / 100);
+        //var elapsed = Math.round(this.state.elapsed / 100);
 
         // This will give a number with one digit after the decimal dot (xx.x):
-        var seconds = (elapsed / 10).toFixed(1);
+        //var seconds = (elapsed / 10).toFixed(1);
+
+        var seconds  = this.state.dataSent;
 
         // Although we return an entire <p> element, react will smartly update
         // only the changed parts, which contain the seconds variable.
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var React = require('react');
+var ReactDOM = require('react-dom/server');
+require('babel-core/register')({
+    presets: ['es2015', 'react']
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+  });
 });
 
 module.exports = router;
